fe6f812d9d6095834394fb80e224f06e288d3ee7
wangshichong <458250970@qq.com>
2017-11-25 19:54:37 +0800

    更新目录结构
    更新目录结构

---
+++ src/main/java/com/smalltomato/demo/Demo.java
@@ -0,0 +1,10 @@
+package com.smalltomato.demo;
+
+/**
+ * @author WangShiChong
+ * @ Create Time: 2017/11/25/0025 19:53
+ */
+
+public class Demo {
+
+}
---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,10 @@
+package java;
+
+/**
+ * @author WangShiChong
+ * @ Create Time: 2017/11/25/0025 19:54
+ */
+
+public class DemoTest {
+
+}
